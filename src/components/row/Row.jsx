1093944bac253c08d6eb1bdb2b9068058a30348d
SUNKUMARM <sunkumar.mayilvel@gmail.com>
2023-08-09 17:01:44 +0530

    row and banner component added

---
+++ src/components/row/Row.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import server from "../../libs/axios";
+import "./row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await server.get(fetchUrl);
+      const { data, status } = response;
+      if (status === 200 && data) {
+        return setMovies(data.results);
+      }
+      return null;
+    };
+    fetchData();
+  }, []);
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row-posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row-single-poster ${isLargeRow && "row-posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt="img"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
