a89c50d314cd5727eee1a7dfa95182914d5d2d50
SUNKUMARM <sunkumar.mayilvel@gmail.com>
2023-08-13 09:49:27 +0530

    landing page multi slides added

--- src/components/multiSlidePage/MultiSlideLandingPage.jsx
+++ src/components/multiSlidePage/MultiSlideLandingPage.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import "./MultiSlideLandingPage.css";
+import "./multiSlideLandingPage.css";
 
-const MultiSlideLandingPage = () => {
-  return <div>MultiSlideLandingPage</div>;
+const MultiSlideLandingPage = ({
+  title,
+  paragraph,
+  className,
+  image,
+  video,
+  tvVideo,
+}) => {
+  return (
+    <div className="multi-slide-parent">
+      <div className={className}>
+        <div className="multi-slide-text">
+          <h1 className="multi-head-title">{title}</h1>
+          <p className="multi-head-description">{paragraph}</p>
+        </div>
+        <div>
+          <div className="image-parent">
+            <img className="slide-image" src={image} alt="tv-img" />
+          </div>
+          {video && (
+            <video className={tvVideo} src={video} autoPlay loop muted></video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MultiSlideLandingPage;
--- src/components/pages/landingPage/LandingPage.jsx
+++ src/components/pages/landingPage/LandingPage.jsx
@@ -4,6 +4,14 @@ import "./landingpage.css";
 import LandingPageNav from "../../navbar/landingPageNav/LandingPageNav";
 import InputFeild from "../../inputFeild/InputFeild";
 import { MdArrowForwardIos } from "react-icons/md";
+import MultiSlideLandingPage from "../../multiSlidePage/MultiSlideLandingPage";
+import tv from "../../../components/assets/tv.png";
+import mobile from "../../../components/assets/strangeThings.jpg";
+import desktop from "../../../components/assets/tvImg2.png";
+import children from "../../../components/assets/children.png";
+import tvVideo1 from "../../../components/assets/tvVideo1.m4v";
+import tvVideo2 from "../../../components/assets/tvVideo2.m4v";
+import MultiSlideDirection from "../../multiSlidePage/multiSlideDirection/MultiSlideDirection";
 
 const LandingPage = () => {
   return (
@@ -45,7 +53,44 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
-      <div>fghjkl</div>
+      <div className="multi-slide-parent-container">
+        <div className="Single-Slide">
+          <MultiSlideLandingPage
+            className="title-content"
+            title="Enjoy on your TV"
+            paragraph="Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more."
+            image={tv}
+            video={tvVideo1}
+            tvVideo="video-one"
+          />
+        </div>
+        <div className="Single-Slide direction">
+          <MultiSlideDirection
+            image={mobile}
+            className="title-content"
+            title="Download your shows to watch offline"
+            paragraph="Save your favourites easily and always have something to watch."
+          />
+        </div>
+        <div className="Single-Slide">
+          <MultiSlideLandingPage
+            title="Watch everywhere"
+            paragraph="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+            className="title-content"
+            image={desktop}
+            video={tvVideo2}
+            tvVideo="video-two"
+          />
+        </div>
+        <div className="Single-Slide direction">
+          <MultiSlideDirection
+            image={children}
+            paragraph="Send children on adventures with their favourite characters in a space made just for them—free with your membership."
+            title="Create profiles for kids"
+            className="title-content"
+          />
+        </div>
+      </div>
     </div>
   );
 };
